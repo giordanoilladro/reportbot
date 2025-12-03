2d91bbd13fd6a95c1e75e387a96c4ca1ceb40d98
ALEXANDERSTEFANELLI <114303048+ALEXANDERSTEFANELLI@users.noreply.github.com>
2025-12-03 16:34:15 +0100

    gg

--- events/ready.js
+++ events/ready.js
@@ -6,8 +6,5 @@ module.exports = {
     console.log(`Bot loggato come ${client.user.tag}!`);
     client.user.setActivity('Report & Verify', { type: 'WATCHING' });
 
-    const leaderboardCmd = require('../commands/leaderboard');
-    await leaderboardCmd.startAllJobs(client);
-    console.log('Leaderboard 22:00 caricata per tutti i server!');
   },
 };
