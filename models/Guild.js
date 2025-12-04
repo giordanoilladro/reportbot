49a448534a4a0b0c000d9f44bd31af323a555c1b
ALEXANDERSTEFANELLI <114303048+ALEXANDERSTEFANELLI@users.noreply.github.com>
2025-12-04 10:44:17 +0100

    gg

--- models/Guild.js
+++ models/Guild.js
@@ -15,10 +15,10 @@ const guildSchema = new mongoose.Schema({
     whitelistChannels: [String],
     allowedDomains: { type: [String], default: ['discord.gg', 'discord.com', 'discordapp.com', 'discord.me'] }
   },
-  messages: { type: Map, of: Number, default: {} }, // userId -> count
-  voiceTime: { type: Map, of: Number, default: {} }   // userId -> seconds
-  channelMessages:     { type: Map, of: Number, default: new Map() },
-  voiceChannelTime:    { type: Map, of: Number, default: new Map() },
+  messages:         { type: Map, of: Number, default: new Map() },
+  voiceTime:        { type: Map, of: Number, default: new Map() },
+  channelMessages:  { type: Map, of: Number, default: new Map() },
+  voiceChannelTime: { type: Map, of: Number, default: new Map() }
 });
 
 module.exports = mongoose.model('Guild', guildSchema);
